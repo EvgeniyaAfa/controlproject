48da43db1b00306aedd6bde0da93e60961f0d83c
EvgeniyaAfa <evgeniya_afanasieva@mail.ru>
2023-01-10 13:39:47 +0300

    final work

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите количество элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] Array = new string [size];
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива под индексом {i} :\t");
+    Array[i] = Convert.ToString(Console.ReadLine());
+
+}
+Console.WriteLine("Вывод массива: ");
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+        Console.WriteLine($" {Array[i]} ");
+}
